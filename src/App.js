e6b652ef59d39a298dc3ee2dd37e08287b1e4d6c
adejoe22 <adedejiomogbehin@gmail.com>
2022-08-12 11:47:17 +0100

    restaurantapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./pages/Navbar";
+import Hero from "./pages/Hero";
+import Platform from "./pages/Platform";
+import Testimonies from "./pages/Testimonies";
+import FAQ from "./FAQ";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Platform />
+      <Testimonies />
+      <FAQ />
+      <Footer />
+    </>
   );
 }
 
